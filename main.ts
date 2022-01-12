2965a3f7dcb3c1bdb4b583b793383ff91d8b47a6
mahtlinux <mahtlinux@gmail.com>
2022-01-12 17:35:58 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 150)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 150)
+})
+input.onButtonPressed(Button.AB, function () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 150)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 150)
+})
+input.onButtonPressed(Button.B, function () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 0)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 150)
+})
 basic.forever(function () {
 	
 })
