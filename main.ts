82503738ab4d6f21d7dc1930d73bd6c5c29494ae
mahtlinux <mahtlinux@gmail.com>
2022-01-13 18:34:50 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,6 +10,12 @@ input.onButtonPressed(Button.B, function () {
     motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 0)
     motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 150)
 })
+let distancia = 0
+makerbit.connectUltrasonicDistanceSensor(DigitalPin.P0, DigitalPin.P1)
 basic.forever(function () {
-	
+    distancia = makerbit.getUltrasonicDistance(DistanceUnit.CM)
+    if (distancia <= 6) {
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 0)
+        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 0)
+    }
 })
